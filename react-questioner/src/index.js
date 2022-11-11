b55586a42e40bf7804774b6efd8d220d5c659b92
koechcoding <koechkelvin2001@gmail.com>
2022-11-11 09:20:19 +0300

    display upcoming meetups

---
+++ react-questioner/src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './js/components/container/routing.jsx';
+
+ReactDOM.render(
+    <Routes/>,
+    document.getElementById('root')
+);
