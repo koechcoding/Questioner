ac72d8311817d7a184a464bda83be0403a449e9e
koechcoding <koechkelvin2001@gmail.com>
2022-11-13 18:06:20 +0300

    display upcoming meetups

--- react-questioner/server.js
+++ react-questioner/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+//connecting
 app.use(express.static(_dirname+'/dist'));
 app.get('*', (req, res) =>{
     res.sendFile(path.resolve(_dirname+'/dist', 'index.html'));
